e4eaac21d014cd9dbb6a4f32df8107e25a96d1e6
Dhruv Baid <dhruvbaid@uchicago.edu>
2021-06-21 13:44:36 -0500

    Initialize orderBook with depthMsg, get lastUpdateId correctly

--- orderBook.py
+++ orderBook.py
@@ -5,22 +5,6 @@ import ast
 import math
 from collections import deque, namedtuple, defaultdict
 
-class Order:
-    def __init__(self, side, size, price, trader, order_id):
-        """
-        Class representing a single order
-        side    : "buy" or "sell"
-        size    : order quantity
-        price   : price in ticks
-        trader  : person who placed order
-        order_id: order ID        
-        """
-        self.side = side
-        self.size = size
-        self.price = price
-        self.trader = trader
-        self.order_id = order_id
-
 class orderBook:
     def __init__(self, name, max_price = 100000, start_order_id = 0):
         """
@@ -38,119 +22,6 @@ class orderBook:
         self.bid_max = 0
         self.ask_min = max_price + 1
 
-    def execute(self, buyer, seller, px, size):
-        print(f"{buyer} bought {size} of {self.name} @ USDT{px} from {seller}.")
-    
-    # limit_order : allows for the placing of orders, as in a typical order book
-    # NOTE: this is what I did to create an order book before realizing that I
-    #       did not actually need to do that for this task... so I believe this
-    #       function can be ignored!
-    def limit_order(self, side, size, px, trader):
-        self.order_id += 1
-        px_tick = int(px/0.0001)
-
-        self.order_id += 1
-
-        if side == "buy":
-            if px_tick > self.bid_max:
-                while px_tick >= self.ask_min:
-                    asks = self.book[self.ask_min]
-                    while asks:
-                        best_ask = asks[0]
-                        if best_ask.size < size:
-                            self.execute(trader,
-                                         best_ask.trader,
-                                         best_ask.px,
-                                         best_ask.size)
-                            size -= best_ask.size
-                            asks.popleft()
-                        elif best_ask.size == size:
-                            self.execute(trader,
-                                         best_ask.trader,
-                                         best_ask.px,
-                                         bst_ask.size)
-                            asks.popleft()
-                            self.order_id += 1
-                            return self.order_id
-                        else:
-                            self.execute(trader,
-                                         best_ask.trader,
-                                         best_ask.px,
-                                         size)
-                            asks[0].size -= size
-                            self.order_id += 1
-                            return self.order_id
-                    
-                    # The incoming bid has hit all of the asks at self.ask_min,
-                    # so we increase self.ask_min and check the next price level
-                    self.ask_min += 1
-                
-                # Incoming bid has filled all asks at prices lower than bid px,
-                # so we add this bid to the queue and update bid_max
-                cur_order = Order(side, size, px_tick, trader, self.order_id)
-                self.book[px_tick].append(cur_order)
-                self.bid_max = max(self.bid_max, px_tick)
-                return self.order_id
-            else:
-                # Incoming bid has lower price than current best bid; just add
-                # it to the queue immediately
-                cur_order = Order(side, size, px_tick, trader, self.order_id)
-                self.book[px_tick].append(cur_order)
-                self.order_id += 1
-                return self.order_id
-        
-        elif side == "sell":
-            if px_tick < self.ask_min:
-                while px_tick <= self.bid_max:
-                    bids = self.book[self.bid_max]
-                    while bids:
-                        best_bid = bids[0]
-                        if best_bid.size < size:
-                            self.execute(best_bid.trader,
-                                         trader,
-                                         best_bid.px,
-                                         best_bid.size)
-                            size -= best_bid.size
-                            bids.popleft()
-                        elif best_bid.size == size:
-                            self.execute(best_bid.trader,
-                                         trader,
-                                         best_bid.px,
-                                         best_bid.size)
-                            bids.popleft()
-                            self.order_id += 1
-                            return self.order_id
-                        else:
-                            self.execute(best_bid.trader,
-                                         trader,
-                                         best_bid.px,
-                                         size)
-                            bids[0].size -= size
-                            self.order_id += 1
-                            return self.order_id
-                    
-                    # The incoming ask has hit all of the bids at self.bid_max,
-                    # so we decrease self.bid_max and check the next price level
-                    self.bid_max -= 1
-                
-                # Incoming ask has filled all bids at prices higher than ask px,
-                # so we add this ask to the queue and update ask_min
-                cur_order = Order(side, size, px_tick, trader, self.order_id)
-                self.book[px_tick].append(cur_order)
-                self.bid_max = min(self.ask_min, px_tick)
-                self.order_id += 1
-                return self.order_id
-            else:
-                # Incoming ask has higher price than current best ask; just add
-                # it to the queue immediately
-                cur_order = Order(side, size, px_tick, trader, self.order_id)
-                self.book[px_tick].append(cur_order)
-                return self.order_id
-                
-        else:
-            print("Order side must be either 'buy' or 'sell'!")
-            return self.order_id
-    
     # update_order : I assume this is what we are supposed to do with the events
     #                we stream from the binance site - update bids and asks to
     #                the quantities we are given, rather than actually placing
@@ -210,20 +81,59 @@ STREAMING_URL = "wss://stream.binance.com:9443/ws/bnbbtc@depth"
 #               documentation.
 #          2. If the criterion listed in the Binance documentation (relating to
 #             update time and lastUpdateId) is met, process this message
-async def stream(uri, orderBook, orderSize):
-    depthMsg = requests.get(DEPTH_API_URL).text
-    lastUpdateId = int(depthMsg.split(",")[0][16:])
+async def stream(uri, orderSize):
+    # Set firstRun flag
+    firstRun = True
+    
+    # Connect to streaming websocket
     async with websockets.connect(uri) as websocket:
         while True:
-            # Obtain the message from the websocket connection
-            msg = await websocket.recv()
+            # On first run, obtain lastUpdateId and initialize orderbook based
+            # on depth message
+            if firstRun:
+                # Get depth message and convert it to a dict
+                depthMsg = ast.literal_eval(requests.get(DEPTH_API_URL).text)
 
-            # Convert the (string) message into a dictionary
-            msg = ast.literal_eval(msg)
+                # Get lastUpdateId
+                lastUpdateId = int(depthMsg["lastUpdateId"])
+                
+                # Get current list of bids and asks
+                bids = [(float(px), float(qty)) for px, qty in depthMsg["bids"]]
+                asks = [(float(px), float(qty)) for px, qty in depthMsg["asks"]]
+
+                # Create empty orderbook
+                myOB = orderBook("BNBBTC")
+
+                # Populate orderbook with current bids and asks (from depth
+                # message)
+                for (px, qty) in bids:
+                    myOB.update_book('buy', px, qty)
+                for (px, qty) in asks:
+                    myOB.update_book('sell', px, qty)
+
+                # Set firstRun flag so that lastUpdateId is not recomputed
+                firstRun = False
+                
+                # Obtain the message from the websocket connection
+                msg = await websocket.recv()
+
+                # Convert the (string) message into a dictionary
+                msg = ast.literal_eval(msg)
             
-            # Check lastUpdateId condition
-            if int(msg["u"]) > lastUpdateId:
-                processMsg(msg, orderBook, orderSize)
+                # Check lastUpdateId condition
+                if int(msg["u"]) > lastUpdateId:
+                    myOB = processMsg(msg, myOB, orderSize)
+
+            else:
+                # Obtain the message from the websocket connection
+                msg = await websocket.recv()
+
+                # Convert the (string) message into a dictionary
+                msg = ast.literal_eval(msg)
+            
+                # Check lastUpdateId condition
+                if int(msg["u"]) > lastUpdateId:
+                    myOB = processMsg(msg, myOB, orderSize)
         return
 
 
@@ -291,7 +201,7 @@ def processMsg(msg, orderBook, orderSize):
     # Reset order size for printing
     orderSize = originalOrderSize
     print(f"For order size {orderSize}, {bidAvg}, {askAvg}", end = "\r")
-    return
+    return orderBook
 
 
 # main : main function which will call appropriate helpers
@@ -299,11 +209,8 @@ async def main():
     # Obtain input order size (allows floats)
     orderSize = float(input("Enter order size: "))
 
-    # Creates new order book
-    myOB = orderBook("BNBBTC")
-
     # Perform computations (until manual termination, i.e. Ctrl + C)
-    await stream(STREAMING_URL, myOB, orderSize)
+    await stream(STREAMING_URL, orderSize)
 
 
 # Run main()
